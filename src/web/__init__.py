ab2e53abbce45e04b35214a1944a5ee749abba45
Benjamin Parsons-Willis <ben@vyrz.dev>
2020-04-23 09:23:16 +0100

    Explained bit uhoh import

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -5,4 +5,6 @@ from . import models
 app = Flask(__name__)
 mongoengine.connect("pulsarc-dev")
 
+
+# Import done after app definition so that routes.py can import the app object
 from . import routes
